3ed0900e68bcd037cdd8d63d2c962a478bb39824
Edu <eborgesfernandez@gmail.com>
2019-09-19 14:31:32 +0100

    HN-237 system check to never try to insert duplicated id's

--- lib/elixir_rtree.ex
+++ lib/elixir_rtree.ex
@@ -17,25 +17,30 @@ defmodule ElixirRtree do
     tree
   end
 
-  def insert(tree,{box,id} = leaf)do
+  def insert(tree,{_box,id} = leaf)do
     meta = tree[:metadata]
-    params = meta[:params]
+    params = meta.params
 
-    recursive_insertion(%{
-      tree: tree,
-      max_width: params[:max_width],
-      min_width: params[:min_width],
-      ets: meta[:ets_table]
-    },'root',id)
-
+    if meta[:ets_table] |> :ets.member(id) do
+      IO.inspect "Impossible to insert, id value already exists: id's MUST be uniq"
+      tree
+    else
+      recursive_insertion(%{
+        tree: tree,
+        max_width: params[:max_width],
+        min_width: params[:min_width],
+        ets: meta.ets_table
+      },'root',leaf)
+    end
   end
 
-  def recursive_insertion(rbundle,key,leaf)do
+  def recursive_insertion(rbundle,key,{box,id})do
 
+    :ets.insert(rbundle.ets,{id,box})
     childs = rbundle.tree |> Map.get(key)
     available_space = rbundle.max_width - length(childs)
     tree_update = case available_space do
-      x when x > 0 -> rbundle.tree |> Map.update!(key,fn ch -> [leaf.id] ++ ch end)
+      x when x > 0 -> rbundle.tree |> Map.update!(key,fn ch -> [id] ++ ch end)
       _ -> IO.puts "Merge"
     end
 
